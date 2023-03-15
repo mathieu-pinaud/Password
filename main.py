b82d3b898a676d79df9639addd289bca86952606
mathieu.pinaud@laplateforme.io <mathieu.pinaud@laplateforme.io>
2023-03-15 23:29:58 +0100

    code fini, commentaires à ajouter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 import os
+import random
 
 def my_json_add(hashed, user):
     fd = open('mdp.json')
@@ -79,7 +80,6 @@ def my_check_data_mdp(password):
             return(False)
     return(True)
 
-
 def my_get_password():
 
     password = input('Veuillez entrer votre mot de passe : ')
@@ -99,6 +99,7 @@ def my_get_password():
     return(password)
 
 def my_get_user():
+
     user = input("Veuillez définir un nom d'utilisateur ")
     if (len(user) == 0):
         return(my_get_user())
@@ -114,9 +115,47 @@ def my_get_user():
             return(my_get_user())
     return(user)
 
+def my_randomizer():
+
+    l_pwd = []
+    pwd = ''
+    maxi_list = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz', '1234567890', '!@#$%^&*']
+    random.seed()
+    l_pwd.append(maxi_list[0][random.randint(0, 25)])
+    l_pwd.append(maxi_list[1][random.randint(0, 25)])
+    l_pwd.append(maxi_list[2][random.randint(0, 9)])
+    l_pwd.append(maxi_list[3][random.randint(0, 7)])
+    for i  in range(4):
+        j = random.randint(0, 3)
+        if j == 0:
+            l_pwd.append(maxi_list[j][random.randint(0, 25)])
+        if j == 1:
+            l_pwd.append(maxi_list[j][random.randint(0, 25)])
+        if j == 2:
+            l_pwd.append(maxi_list[j][random.randint(0, 9)])
+        if j == 3:
+            l_pwd.append(maxi_list[j][random.randint(0, 7)])
+    random.shuffle(l_pwd)
+    for i in l_pwd:
+        pwd += i
+    print("Votre mot de passe est", pwd, "veillez à le conserver")
+    return(pwd)
+
 def my_password_handler():
+
     user = my_get_user()
-    password = my_get_password()
+    try:
+        i = int(input("Tapez '1' pour créer vous meme votre mot de passe ou '2' pour le créer aléatoirement : "))
+    except:
+        print('Saisie incorrecte')
+        return()
+    if (i == 1):
+        password = my_get_password()
+    elif (i == 2):
+        password = my_randomizer()
+    else:
+        print('Saisie incorrecte')
+        return()
     hashed = hashlib.sha256(password.encode()).hexdigest()
     my_json_starter(hashed , user)
 
@@ -132,6 +171,7 @@ def my_data_reader():
         print("le mot de passe (crypté) associé à l'utilisateur", key, 'est :', data[key])
     
 def my_del_json():
+
     try:
         open('mdp.json')
     except:
@@ -140,6 +180,7 @@ def my_del_json():
         os.remove('mdp.json')
 
 def my_connect(data, key):
+
     test = data[key]
     cpt = 0
     while(cpt != 3):
@@ -155,11 +196,12 @@ def my_connect(data, key):
 
         
 def my_del_user():
+
     del_usr = input("Renseigner le nom d'utilisateur a supprimer : ")
     try:
         fd = open('mdp.json')
     except:
-        print("pas de base de données a modifier")
+        print("Pas de base de données a modifier")
         return()
     data = json.load(fd)
     fd.close
@@ -173,29 +215,51 @@ def my_del_user():
                 return()
             else:
                 print("Vous n'avez pas les droits sur cet utilisateur")
+                return()
     print("Le nom d'utilisateur donné n'existe pas dans la base de données")
-    
+
+def my_modif_mdp():
+
+    try:
+        fd = open('mdp.json')
+        data = json.load(fd)
+        fd.close()
+    except:
+        print('Pas de base de données à modifier')
+        return()
+    usr = input("De quel utilisateur souhaitez-vous modifier le mot de passe ? ")
+    for key in data:
+        if (key == usr):
+            if (my_connect(data, key)):
+                new_pswrd = my_get_password()
+                new_pswrd = hashlib.sha256(new_pswrd.encode()).hexdigest()
+                my_json_add(new_pswrd, key)
+                return()
+            else:
+                print("Vous n'avez pas les droits sur cet utilisateur")
+                return()
+    print("Le nom d'utilisateur donné n'existe pas dans la base de données")
+
 def my_menu():
+
     i = 0
+    method_list = [my_password_handler, my_data_reader, my_del_json, my_del_user, my_modif_mdp]
+
     print("-Tapez 1 pour enregistrer un mot de passe")
     print("-Tapez 2 pour afficher les mots de passe")
     print("-Tapez 3 pour supprimer la base données")
     print("-Tapez 4 pour supprimer un utilisateur")
-    print("-Tapez 5 pour sortir du programme")
-    while (i != 5):
+    print("-Tapez 5 pour modifier un mot de passe")
+    print("-Tapez 6 pour sortir du programme")
+    
+    while (i != 6):
         try :
             i = int(input("Selection : "))
         except ValueError:
-            i = 0
-        if (i == 1):
-            my_password_handler()
-        if (i == 2):
-            my_data_reader()
-        if (i == 3):
-            my_del_json()
-        if (i == 4):
-            my_del_user()
-        elif ( i <= 0 or i > 5):
-            print("La saisie est invalide")
+            i = -1
+        if (i > 0 and i <= 5):
+            method_list[i - 1]()
+        elif (i != 6):
+            print("Saisie invalide")
 
 my_menu()
